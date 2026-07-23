cf137e7dbb2cb5858391a0975422aa90472fb170
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-23 14:49:57 +0500

    Add utility functions for script control

--- core/utils.sh
+++ core/utils.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+pause() {
+    echo
+    read -rp "Press Enter to continue..."
+}
+
+line() {
+    echo "--------------------------------------"
+}
+
+header() {
+    clear
+    banner
+    line
+}
