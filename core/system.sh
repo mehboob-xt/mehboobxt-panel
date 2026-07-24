31c4d7f7cb2e3af1c84b2e640354043a18b8f45e
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-24 16:13:22 +0500

    feat(core): add system helper framework

---
+++ core/system.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# ===================================
+# MehboobXT System Helper
+# Linux System Operations
+# ===================================
+system_user_exists() {
+
+}
+
+system_create_user() {
+
+}
+
+system_delete_user() {
+
+}
+
+system_change_password() {
+
+}
+
+system_lock_user() {
+
+}
+
+system_unlock_user() {
+
+}
+
+system_rename_user() {
+
+}
+
+system_set_expiry() {
+
+}
+
+system_online_users() {
+
+}
