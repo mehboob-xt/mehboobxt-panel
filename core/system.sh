e95a7a8d52335e5912946837a3b4f2ec4a9fb67d
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-24 16:17:52 +0500

    feat(core): implement system helper functions

--- core/system.sh
+++ core/system.sh
@@ -6,36 +6,81 @@
 # ===================================
 system_user_exists() {
 
+    local USER="$1"
+
+    id "$USER" >/dev/null 2>&1
+
 }
 
 system_create_user() {
+
+    local USER="$1"
+    local PASS="$2"
+    local EXPIRY="$3"
+
+    useradd \
+        -e "$EXPIRY" \
+        -M \
+        -s /usr/sbin/nologin \
+        "$USER" || return 1
+
+    echo "$USER:$PASS" | chpasswd || return 1
 
 }
 
 system_delete_user() {
 
+    local USER="$1"
+
+    userdel -f "$USER"
+
 }
 
 system_change_password() {
+
+    local USER="$1"
+    local PASS="$2"
+
+    echo "$USER:$PASS" | chpasswd
 
 }
 
 system_lock_user() {
 
+    local USER="$1"
+
+    passwd -l "$USER"
+
 }
 
 system_unlock_user() {
+
+    local USER="$1"
+
+    passwd -u "$USER"
 
 }
 
 system_rename_user() {
 
+    local OLDUSER="$1"
+    local NEWUSER="$2"
+
+    usermod -l "$NEWUSER" "$OLDUSER"
+
 }
 
 system_set_expiry() {
 
+    local USER="$1"
+    local EXPIRY="$2"
+
+    usermod -e "$EXPIRY" "$USER"
+
 }
 
 system_online_users() {
+
+    who
 
 }
