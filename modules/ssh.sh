c23a720846e3d463d8eaa15b7865e7822a46d60c
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-24 08:12:20 +0500

    Complete Premium SSH list and show user modules

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -152,24 +152,30 @@ if id "$USER" >/dev/null 2>&1; then
 fi
 
 echo ""
-success "SSH User Created Successfully"
-
-echo ""
-echo "Username : $USER"
-echo "Password : $PASS"
-echo "Expiry   : $EXPIRY"
+# Create Linux SSH User
 # Expiry Date
 EXPIRY=$(date -d "$DAYS days" +"%Y-%m-%d")
 
-# Create Linux SSH User
 useradd \
 -e "$EXPIRY" \
 -M \
 -s /usr/sbin/nologin \
 "$USER"
 
-# Set Password
 echo "$USER:$PASS" | chpasswd
+
+echo "$USER|$PASS|$EXPIRY" >> "$DB"
+
+echo ""
+success "SSH User Created Successfully"
+
+echo ""
+echo "Username : $USER"
+echo "Password : $PASS"
+echo "Expiry   : $EXPIRY"
+
+pause
+# Create Linux SSH User
 
     pause
 
@@ -177,9 +183,68 @@ echo "$USER:$PASS" | chpasswd
 
 list_ssh_users() {
 
+    header
+
+    echo "========== SSH Users =========="
+    echo ""
+
+    if [ ! -s "$DB" ]; then
+        error "No SSH users found"
+        pause
+        return
+    fi
+
+    while IFS="|" read -r USER PASS EXPIRY
+    do
+        echo "Username : $USER"
+        echo "Password : $PASS"
+        echo "Expiry   : $EXPIRY"
+        echo "-----------------------------"
+    done < "$DB"
+
+    pause
+
 }
 
 show_ssh_user() {
+
+    header
+
+    echo ""
+echo "========== SSH User =========="
+echo ""
+echo "Username : $USER"
+echo "Password : $PASS"
+echo "Expiry   : $EXPIRY"
+echo "=============================="
+echo ""
+    read -rp "Username : " USER
+
+    if [ ! -f "$DB" ]; then
+        error "Database not found"
+        pause
+        return
+    fi
+
+    DATA=$(grep -m1 "^$USER|" "$DB")
+
+    if [ -z "$DATA" ]; then
+        error "SSH User not found"
+        pause
+        return
+    fi
+
+    IFS="|" read -r USER PASS EXPIRY <<< "$DATA"
+
+    echo ""
+    success "SSH User Found"
+    echo ""
+
+    echo "Username : $USER"
+    echo "Password : $PASS"
+    echo "Expiry   : $EXPIRY"
+
+    pause
 
 }
 
