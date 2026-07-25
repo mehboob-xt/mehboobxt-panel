ddf5bf984d46b6c43cf74f596b90bcacb19b0c18
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-25 13:58:18 +0500

    Implement non-TLS and TLS proxy config generation

--- core/config_generator.sh
+++ core/config_generator.sh
@@ -10,17 +10,24 @@ generate_tls_config() {
 }
 
 generate_non_tls_config() {
-    :
+
+    local SERVER="${SERVER:-${SERVER_DOMAIN:-your-domain.com}}"
+    local PORT="${PORT:-80}"
+    local HOST="${HOST:-$SERVER}"
+    local PATH_WS="${PATH_WS:-/ws}"
+
+    echo "trojan://${PASSWORD}@${SERVER}:${PORT}?security=none&type=ws&host=${HOST}&path=${PATH_WS}#${NAME}"
+
 }
 
 generate_tls_proxy_config() {
-    :
+
+    local SERVER="${SERVER:-${SERVER_DOMAIN:-your-domain.com}}"
+    local PORT="${PORT:-443}"
+    local HOST="${HOST:-$SERVER}"
+    local PATH_WS="${PATH_WS:-/ws}"
+
+    echo "trojan://${PASSWORD}@${SERVER}:${PORT}?security=tls&type=ws&host=${HOST}&path=${PATH_WS}&mode=proxy#${NAME}"
+
 }
 
-generate_httpcustom_config() {
-    :
-}
-
-generate_darktunnel_config() {
-    :
-}
