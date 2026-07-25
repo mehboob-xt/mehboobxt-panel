2ea47cf6ce977d7a2343c3e64a176598631bad80
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-25 13:29:58 +0500

    Add placeholder functions for config generation

--- core/config_generator.sh
+++ core/config_generator.sh
@@ -1,1 +1,19 @@
+generate_tls_config() {
+    :
+}
 
+generate_non_tls_config() {
+    :
+}
+
+generate_tls_proxy_config() {
+    :
+}
+
+generate_httpcustom_config() {
+    :
+}
+
+generate_darktunnel_config() {
+    :
+}
