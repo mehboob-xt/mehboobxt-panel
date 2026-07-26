a43292a76ae1c565f9b9a63ff209f4753792826b
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-26 14:53:03 +0500

    Add shebang to installer script
    Replaced installation steps with a shebang.

--- installer/install.sh
+++ installer/install.sh
@@ -1,17 +1,1 @@
-Start
-│
-├── Root Check
-├── check_os.sh
-├── check_network.sh
-├── system_update.sh
-├── install_dependencies.sh
-├── create_directories.sh
-├── create_databases.sh
-├── create_config.sh
-├── download_panel.sh
-├── set_permissions.sh
-├── create_service.sh
-├── create_commands.sh
-├── finish_install.sh
-│
-└── End
+#!/bin/bash
