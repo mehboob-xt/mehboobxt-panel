67499a04665a875ea57bf3bfb1464365b8c3e3db
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-27 17:20:09 +0500

    Refactor OS check script for clarity and support

--- installer/check_os.sh
+++ installer/check_os.sh
@@ -1,126 +1,39 @@
 #!/bin/bash
 
 set -e
-set -u
-set -o pipefail
 
-info() {
-    echo -e "\033[1;34m[INFO]\033[0m $1"
-}
+source "$(dirname "$0")/../core/config.sh"
+source "$(dirname "$0")/../core/logger.sh"
 
-success() {
-    echo -e "\033[1;32m[ OK ]\033[0m $1"
-}
-
-warning() {
-    echo -e "\033[1;33m[WARN]\033[0m $1"
-}
-
-error() {
-    echo -e "\033[1;31m[FAIL]\033[0m $1"
-}
-
-########################################
-# Check OS File
-########################################
+info "Checking operating system..."
 
 if [[ ! -f /etc/os-release ]]; then
-    error "/etc/os-release not found."
+    error "Unsupported operating system."
     exit 1
 fi
 
 source /etc/os-release
 
-########################################
-# Supported Operating Systems
-########################################
-
 case "$ID" in
-
-    ubuntu)
-
-        case "$VERSION_ID" in
-            20.04|22.04|24.04)
-                success "Ubuntu $VERSION_ID detected."
-                ;;
-            *)
-                error "Unsupported Ubuntu version: $VERSION_ID"
-                exit 1
-                ;;
-        esac
+    ubuntu|debian)
+        success "$PRETTY_NAME detected."
         ;;
-
-    debian)
-
-        case "$VERSION_ID" in
-            11|12)
-                success "Debian $VERSION_ID detected."
-                ;;
-            *)
-                error "Unsupported Debian version: $VERSION_ID"
-                exit 1
-                ;;
-        esac
-        ;;
-
     *)
-
-        error "Unsupported operating system: $ID"
-
+        error "Only Ubuntu and Debian are supported."
         exit 1
-
         ;;
-
 esac
-
-########################################
-# Architecture
-########################################
 
 ARCH=$(uname -m)
 
 case "$ARCH" in
-
-    x86_64)
-
-        success "Architecture : x86_64"
-
+    x86_64|amd64|aarch64|arm64)
+        success "Architecture: $ARCH"
         ;;
-
-    aarch64|arm64)
-
-        success "Architecture : ARM64"
-
-        ;;
-
     *)
-
-        error "Unsupported architecture : $ARCH"
-
+        error "Unsupported architecture: $ARCH"
         exit 1
-
         ;;
-
 esac
 
-########################################
-# Kernel
-########################################
-
-KERNEL=$(uname -r)
-
-info "Kernel : $KERNEL"
-
-########################################
-# Hostname
-########################################
-
-HOST=$(hostname)
-
-info "Hostname : $HOST"
-
-########################################
-# Finish
-########################################
-
-success "Operating System check completed."
+exit 0
