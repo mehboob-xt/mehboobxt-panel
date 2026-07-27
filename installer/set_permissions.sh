f153bc88eaad1d9ede3a0ac7efc022b0d7172a9e
Mehboob XT <mehbooblucky8757@gmail.com>
2026-07-27 17:12:47 +0500

    Refactor set_permissions.sh to streamline permissions

--- installer/set_permissions.sh
+++ installer/set_permissions.sh
@@ -1,106 +1,26 @@
 #!/bin/bash
 
 set -e
-set -u
-set -o pipefail
 
-info() {
-    echo -e "\033[1;34m[INFO]\033[0m $1"
-}
+source "$(dirname "$0")/../core/config.sh"
+source "$(dirname "$0")/../core/logger.sh"
 
-success() {
-    echo -e "\033[1;32m[ OK ]\033[0m $1"
-}
+info "Setting MehboobXT permissions..."
 
-error() {
-    echo -e "\033[1;31m[FAIL]\033[0m $1"
-}
+chmod -R 755 "$PANEL_DIR"
+chmod -R 700 "$CONFIG_DIR"
+chmod -R 700 "$DATA_DIR"
+chmod -R 755 "$MODULE_DIR"
+chmod -R 755 "$CORE_DIR"
+chmod -R 755 "$ASSETS_DIR"
+chmod -R 755 "$TEMPLATES_DIR"
+chmod -R 700 "$LOG_DIR"
+chmod -R 700 "$BACKUP_DIR"
+chmod -R 755 "$EXPORT_DIR"
+chmod -R 755 "$CACHE_DIR"
+chmod -R 700 "$SESSION_DIR"
 
-########################################
-# Root Check
-########################################
-
-if [[ $EUID -ne 0 ]]; then
-    error "Please run as root."
-    exit 1
-fi
-
-########################################
-# Paths
-########################################
-
-PANEL_DIR="/usr/local/mehboobxt"
-
-CONFIG_DIR="/etc/mehboobxt"
-
-DATA_DIR="/var/lib/mehboobxt"
-
-LOG_DIR="/var/log/mehboobxt"
-
-########################################
-# Verify
-########################################
-
-for dir in \
-"$PANEL_DIR" \
-"$CONFIG_DIR" \
-"$DATA_DIR" \
-"$LOG_DIR"
-do
-
-    if [[ ! -d "$dir" ]]; then
-        error "$dir not found."
-        exit 1
-    fi
-
-done
-
-########################################
-# Ownership
-########################################
-
-info "Setting ownership..."
-
-chown -R root:root "$PANEL_DIR"
-chown -R root:root "$CONFIG_DIR"
-chown -R root:root "$DATA_DIR"
-chown -R root:root "$LOG_DIR"
-
-success "Ownership configured."
-
-########################################
-# Directory Permissions
-########################################
-
-find "$PANEL_DIR" -type d -exec chmod 755 {} \;
-find "$CONFIG_DIR" -type d -exec chmod 755 {} \;
-find "$DATA_DIR" -type d -exec chmod 755 {} \;
-find "$LOG_DIR" -type d -exec chmod 755 {} \;
-
-success "Directory permissions configured."
-
-########################################
-# File Permissions
-########################################
-
-find "$PANEL_DIR" -type f -exec chmod 644 {} \;
-find "$CONFIG_DIR" -type f -exec chmod 600 {} \;
-find "$DATA_DIR" -type f -exec chmod 600 {} \;
-find "$LOG_DIR" -type f -exec chmod 644 {} \;
-
-success "File permissions configured."
-
-########################################
-# Executable Scripts
-########################################
-
-find "$PANEL_DIR" -name "*.sh" -exec chmod +x {} \;
-
-success "Shell scripts marked executable."
-
-########################################
-# Finish
-########################################
+find "$PANEL_DIR" -type f -name "*.sh" -exec chmod +x {} \;
 
 success "Permissions configured successfully."
 
